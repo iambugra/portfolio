0f162b9b07d1b2bd24612c34a54406f656a504aa
Bugra Alparslan <iambugraalparslan@gmail.com>
2024-01-16 12:54:25 +0300

    load js

--- static/js/script.js
+++ static/js/script.js
@@ -1,5 +1,6 @@
 console.log('Siuuuuuu')
 
+let basePath = "/static/css/"
 let theme = localStorage.getItem('theme')
 
 if (theme == null){
@@ -20,19 +21,19 @@ for (var i=0; i < themeDots.length; i++) {
 
 function setTheme(mode) {
     if (mode=='light'){
-        document.getElementById('theme-style').href = 'default.css'
+        document.getElementById('theme-style').href = basePath + 'default.css'
     }
 
     if (mode=='blue'){
-        document.getElementById('theme-style').href = 'blue.css'
+        document.getElementById('theme-style').href = basePath + 'blue.css'
     }
 
     if (mode=='purple'){
-        document.getElementById('theme-style').href = 'purple.css'
+        document.getElementById('theme-style').href = basePath + 'purple.css'
     }
 
     if (mode=='green'){
-        document.getElementById('theme-style').href = 'green.css'
+        document.getElementById('theme-style').href = basePath + 'green.css'
     }
 
     localStorage.setItem('theme', mode)
