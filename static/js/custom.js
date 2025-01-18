f933320ee55f28ce1cf0bbd3ccba7126c0823724
Bugra Alparslan <iambugraalparslan@gmail.com>
2025-01-19 00:35:55 +0300

    Make style changes on carousel

--- static/js/custom.js
+++ static/js/custom.js
@@ -13,10 +13,25 @@
 
     // PROJECT CAROUSEL
     $('.owl-carousel').owlCarousel({
-    	items: 1,
+    	items: 3,
 	    loop:true,
 	    margin:10,
-	    nav:true
+	    nav:true,
+      dots: true,
+      autoplay: true,
+      autoplayTimeout: 4000,
+      autoplayHoverPause: true,
+      responsive: {
+          0: {
+              items: 1
+          },
+          600: {
+              items: 2
+          },
+          1000: {
+              items: 3
+          }
+      }
 	});
 
     // SMOOTHSCROLL
